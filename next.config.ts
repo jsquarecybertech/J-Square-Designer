50fb6745aa247fe235a0c914a36c4ed5b8e020dc
jsquarecybertech <jsquarecybertech@gmail.com>
2026-07-20 00:35:26 +0530

    Remove all files

--- next.config.ts
+++
@@ -1,8 +0,0 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-  // Netlify's Next.js runtime automatically optimizes image serving and caching
-};
-
-export default nextConfig;
